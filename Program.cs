cf72fba1cbb011e819426926588b92c6d94243b9
Gabriel Dutra <gabrielinvst1@gmail.com>
2024-11-07 17:46:06 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -16,10 +16,18 @@ hospedes.Add(p2);
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+try
+{
+    reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+    // Exibe a quantidade de hóspedes e o valor da diária
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+}
+catch (Exception)
+{
+    Console.WriteLine("Número de pessoas excedeu o limite");
+}
+
